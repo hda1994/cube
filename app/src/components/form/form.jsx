171b6b79fb6c1cafb6af3e2ea05abfa05edac880
hda <hda1994@gmail.com>
2020-12-21 23:09:34 +0400

    change input color

--- app/src/components/form/form.jsx
+++ app/src/components/form/form.jsx
@@ -40,6 +40,7 @@ export function Form(props) {
                     InputLabelProps={{
                         shrink: true,
                     }}
+                    color="secondary"
                     variant="outlined"
                     onChange={e => setHeight(e.target.value)}
                 />
@@ -50,6 +51,7 @@ export function Form(props) {
                     InputLabelProps={{
                         shrink: true,
                     }}
+                    color="secondary"
                     variant="outlined"
                     onChange={e => setWidth(e.target.value)}
                 />
@@ -60,6 +62,7 @@ export function Form(props) {
                     InputLabelProps={{
                         shrink: true,
                     }}
+                    color="secondary"
                     variant="outlined"
                     onChange={e => setLength(e.target.value)}
                 />
